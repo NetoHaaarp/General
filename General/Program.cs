d16511754f1eae481859a84d237a11f0fe1dd889
NetoHaarp <NetoHaarp@LENOVO-PCNETO>
2017-11-13 22:22:13 -0600

    Agregar archivos de proyecto.

---
+++ General/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace prog
+{
+    class General
+    {
+        public static void Main(String[] args)
+        {
+            double x1 = 0;
+            double x2 = 0;
+            Console.WriteLine("Formula General");
+            Console.WriteLine("Inserte a");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Inserte b");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Inserte c");
+            double c = double.Parse(Console.ReadLine());
+            c = ((b * b) - (4) * (a) * (c));
+            if (c < 0)
+            {
+                Console.WriteLine("Error");
+            }
+            else
+            {
+                c = Math.Sqrt(c);
+                x1 = (((-1) * (b)) + c) / (2 * (a));
+                x2 = (((-1) * (b)) - c) / (2 * (a));
+                Console.WriteLine("X1 = " + x1);
+                Console.WriteLine("x2 = " + x2);
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
